0b10dee51a0e40253434a02045dabe91cf1dc052
Loka017 <jessinthespace@gmail.com>
2021-09-12 07:49:41 +0900

    Intent 이용 - > 다른화면으로 이동

--- app/src/main/java/com/ateam/intentpractice_loka/MainActivity.kt
+++ app/src/main/java/com/ateam/intentpractice_loka/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ateam.intentpractice_loka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//            다른화면으로 이동(to OtherAtivity)
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
